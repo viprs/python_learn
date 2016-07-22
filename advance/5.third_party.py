7e4d9bf82afb19bead2a174b73f26454d00f80cf
牛棚琐思 <viprs@qq.com>
2016-07-22 16:46:04 +0800

    Python第三方有用的库

---
+++ advance/5.third_party.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding:utf-8
+
+# 1. NumPy: 一个开源的相当于MatLab的包
+# 2. SciPy: 另一个数值处理库
+# 3. GPULib: 使用GPUs加速代码
+# 4. PyPy: 使用 just-in-time 编译器优化Python代码
+# 5. Cython: 將Python优码转成C
+# 6. ShedSkin: 將Python代码转成C++
